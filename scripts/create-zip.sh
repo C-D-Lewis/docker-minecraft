20728609c6eeb557faaae1d80a340cd2fb790eb1
Chris Lewis <Bonsitm@gmail.com>
2024-09-22 23:12:53 +0100

    update

--- scripts/create-zip.sh
+++ scripts/create-zip.sh
@@ -15,7 +15,7 @@ echo ">>> Updating ownership"
 chown -R $USR ./
 
 echo ">>> Creating zip"
-zip -r $OUTPUT_FILE . --exclude "*node_modules*" --exclude "*backups*" --exclude "*dynmap*"
+zip -r $OUTPUT_FILE . --exclude "*node_modules*" --exclude "*backups*" --exclude "*plugins*"
 
 size=$(stat -c '%s' $OUTPUT_FILE | numfmt --to=si --suffix=B)
 echo ">>> Size: $size"
